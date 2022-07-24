db27624b0de5c522c62e8ef22209de9f5436f00a
JEES23 <76464398+JEES23@users.noreply.github.com>
2022-07-24 23:13:21 +0900

    section 1.
    주석 추가

--- code/websever.js
+++ code/websever.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const http = require('http');       //http 모듈 가져와 http 상수에 넣기
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {    //서버 생성 + listen 호출 가능(객체지향)
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World');     //응답 결과 : Hello World 출력
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, hostname, () => {   //3000번 포트를 listen하는 서버를 만들고, 127.0.0.1로 접속한 사용자에 대해 응답하라
   console.log(`Server running at http://${hostname}:${port}/`);
 });
